7b0cec07ffcfcb701bbad6489933a0ac1334a3c8
aburbank <aburbank@uci.edu>
2021-02-24 22:53:31 -0800

    Added basic functionalitites to Tile.java

--- src/GameData/Tile.java
+++ src/GameData/Tile.java
@@ -1,5 +1,41 @@
-import java.util.*
+import java.util.*;
 
 class Tile {
-	
+
+	private int x;
+	private int y;
+	private String color;
+
+	Tile(int newX, int newY, String newColor) {
+		this.x = newX;
+		this.y = newY;
+		this.color = newColor; 
+	}
+
+	public void setX(int x){
+		this.x = x;
+	}
+
+	public void setY(int y){
+		this.y = y;
+	}
+
+	public void setColor(String color){
+		this.color = color;
+	}
+
+	public int getX(){
+		return this.x;
+	}
+
+	public int getY(){
+		return this.y; 
+	}
+
+	public String getColor(){
+		return this.color; 
+	}
+
+
+
 }

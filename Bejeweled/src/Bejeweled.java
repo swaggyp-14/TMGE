b0b84b35cdfe9883d4630323eb57825ffc7859bc
HJ <ericj1114@gmail.com>
2021-03-02 17:46:24 -0800

    initial bejeweled impl

--- Bejeweled/src/Bejeweled.java
+++ Bejeweled/src/Bejeweled.java
@@ -6,7 +6,13 @@ public class Bejeweled {
     private String gameName = "Bejeweled";
     private EngineAPI e;
     public JFrame getStage() throws Exception {
-
+        // TODO - Add more customizations etc later
+        e = new EngineAPI();
+        e.setTileDimensions(8, 8);
+        e.initGame(gameName);
+        e.setGameScreenSize(1000, 800);
+        e.setGamePanels();
+        e.setScorePerClear(150);
         return e.getFrame();
     }
 }
